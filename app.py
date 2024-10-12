ae0d7f8d0dda905b6098e3b7d139ae0512bc5be2
n3ohck.mx <alexasa.wdc@gmail.com>
2024-10-12 12:06:17 -0600

    Se agrega la lógica de la práctica flask contactos con conexion a MongoDB y serializacion de los datos

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+from bson import ObjectId
+
 
 app = Flask(__name__)
 
-# Lista de contactos (puedes reemplazar esto por una base de datos más adelante)
-contactos = [
-    {"nombre": "Juan Pérez", "telefono": "123456789"},
-    {"nombre": "María García", "telefono": "987654321"},
-    {"nombre": "Ana López", "telefono": "456123789"},
-    {"nombre": "Kaleb Kirabelak", "telefono": "456123789"},
-]
+# Reemplaza 'miUsuario' y 'miContraseña' con tus credenciales
+username = quote_plus('n3ohck')  # Escapar el nombre de usuario
+password = quote_plus('Kvm82645699@@')  # Escapar la contraseña
+cluster_url = 'n3ohck.2bnezap.mongodb.net'
+db_name = 'agenda'
+
+# Configuración de la conexión a MongoDB
+connection_string = f'mongodb+srv://{username}:{password}@{cluster_url}/{db_name}?retryWrites=true&w=majority'
+client = MongoClient(connection_string)
+db = client[db_name]
+
+contactos_collection = db['contactos']
+
+def serialize_contacto(contacto):
+    """Función para serializar el contacto"""
+    contacto['_id'] = str(contacto['_id'])  # Convertir ObjectId a string
+    return contacto
 
 @app.route('/')
 def index():
@@ -17,16 +31,23 @@ def index():
 @app.route('/buscar', methods=['GET'])
 def buscar_contacto():
     query = request.args.get('query', '')
-    resultados = [contacto for contacto in contactos if query.lower() in contacto['nombre'].lower()]
-    return jsonify(resultados)
+    resultados = list(contactos_collection.find({"nombre": {"$regex": query, "$options": "i"}}))
+    resultados_serializados = [serialize_contacto(contacto) for contacto in resultados]
+    return jsonify(resultados_serializados)
 
 @app.route('/agregar', methods=['POST'])
 def agregar_contacto():
+    nombre = request.form.get('nombre')
+    telefono = request.form.get('telefono')
+
+    if contactos_collection.find_one({"nombre": nombre}):
+        return jsonify({"mensaje": "El contacto ya existe."}), 400  # Código de error 400 para solicitud incorrecta
+
     nuevo_contacto = {
-        "nombre": request.form.get('nombre'),
-        "telefono": request.form.get('telefono')
+        "nombre": nombre,
+        "telefono": telefono
     }
-    contactos.append(nuevo_contacto)
+    contactos_collection.insert_one(nuevo_contacto)
     return jsonify({"mensaje": "Contacto agregado con éxito!"}), 201
 
 if __name__ == '__main__':
